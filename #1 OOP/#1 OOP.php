a7df4a522e9d25eece6b9beb3da9df9212966a53
HoboHub <cavemanclif@mail.ru>
2021-02-10 12:21:52 +0300

    Update #1 OOP.php

--- #1 OOP/#1 OOP.php
+++ #1 OOP/#1 OOP.php
@@ -1,4 +1,7 @@
 <?php
+$mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
+
+// класс Item не наследуется. В конструктор класса передается ID объекта.
 class Item
 {
   private int $id;
@@ -14,14 +17,16 @@ class Item
     $this->changed = $changed;
   }
  
+  //Метод получает из таблицы objects данные name и status и заполняет их в свойства экземпляра
   private function init()
   {
-    // $name = $mysql->query("SELECT `name` FROM `objects`")->fetch_assoc();
-    // $status = $mysql->query("SELECT `status` FROM `objects`")->fetch_assoc();
+    $name = $mysqli->query("SELECT `name` FROM `objects`")->fetch_assoc();
+    $status = $mysqli->query("SELECT `status` FROM `objects`")->fetch_assoc();
     $this->name = $name;
     $this->status = $status;
   }
  
+  //получение свойств объекта, используя magic method
   public function __get($key)
   {
     return $this->key = $key
@@ -32,9 +37,24 @@ class Item
     $this->$key = $value;
   }
 
+  //Метод сохраняет установленные значения name и status в случае, если свойства объекта были изменены
   public function save() 
   {
-    //code..
+	$state = $mysqli->prepare("INSERT INTO `objects` VALUES (?,?)");
+
+	$name = $_POST["name"];
+	$status = $_POST["status"];
+	$state->bind_param('ss', $name, $status);
+
+	$state->execute();
+	if ($state->affected_rows != 0) {
+	    var_dump($name, $status);
+	    echo 'Значение добавлено!';   
+	};
+
+	$state->close();
   }
 }
+
+$mysqli->close();
 ?>
